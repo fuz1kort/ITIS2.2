7875f85221a47a786e8912504537067207f71593
Marat Gaffarov <gaffarov142004@gmail.com>
2024-03-06 16:37:39 +0300

    need a pagination to search

--- pokemon-app/src/views/SearchPage.jsx
+++ pokemon-app/src/views/SearchPage.jsx
@@ -1,78 +1,91 @@
 import SearchHeader from "../components/SearchHeader";
-import PokemonsList from "../components/PokemonsList";
 import React, {useEffect, useState} from "react";
+import PokemonsList from "../components/PokemonsList";
 
 const SearchPage = () => {
-        const [allPokemons, setAllPokemons] = useState([]);
-        const [searchText, setSearchText] = useState('');
-        const [searchResults, setSearchResults] = useState([]);
-        const [limit, setLimit] = useState(65);
-        const [fetching, setFetching] = useState(true)
+    const [allPokemons, setAllPokemons] = useState([]);
+    const [searchResults, setSearchResults] = useState([]);
+    const [searchText, setSearchText] = useState('');
+    const [offset, setOffset] = useState(0);
+    const [fetching, setFetching] = useState(true)
 
-        useEffect(() => {
-            document.addEventListener('scroll', scrollHandler)
+    useEffect(() => {
+        document.addEventListener('scroll', scrollHandler)
 
-            return function () {
-                document.removeEventListener('scroll', scrollHandler)
-            }
-        })
-
-        const scrollHandler = (e) => {
-            if (e.target.documentElement.scrollHeight - (e.target.documentElement.scrollTop + window.innerHeight) < 100 && !fetching) {
-                setFetching(true)
-            }
+        return function () {
+            document.removeEventListener('scroll', scrollHandler)
         }
+    })
 
-        useEffect(() => {
-            if (fetching) {
-                fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}`)
-                    .then(response => response.json())
-                    .then(data => {
-                        data.results.forEach(pokemon => fetchPokemonData(pokemon));
-                        setLimit(limit + 65)
-                    })
-                    .catch(error => console.error('Error fetching initial data:', error))
-                    .finally(() => setFetching(false));
-            }
-        }, [fetching, limit]);
+    const scrollHandler = (e) => {
+        if (e.target.documentElement.scrollHeight - (e.target.documentElement.scrollTop + window.innerHeight) < 600 && !fetching) {
+            setFetching(true)
+        }
+    }
 
-        const fetchPokemonData = async (pokemon) => {
-            fetch(pokemon.url)
+    useEffect(() => {
+        if (fetching) {
+            fetch(`https://pokeapi.co/api/v2/pokemon?limit=60&offset=${offset}`)
                 .then(response => response.json())
-                .then(pokemonData => {
-                    setAllPokemons(prevAllPokemons => ({
+                .then(data => {
+                    data.results.forEach(pokemon => fetchPokemonData(pokemon));
+                    setOffset(prevOffset => prevOffset + 30);
+                })
+                .catch(error => console.error('Error fetching initial data:', error))
+                .finally(() => setFetching(false));
+
+        }
+    }, [fetching, offset]);
+
+    const fetchPokemonData = async (pokemon) => {
+        fetch(pokemon.url)
+            .then(response => response.json())
+            .then(pokemonData => {
+                setAllPokemons(prevAllPokemons => ({
+                    ...prevAllPokemons,
+                    [pokemonData.id]: pokemonData
+                }));
+                if (!searchText) {
+                    setSearchResults(prevAllPokemons => ({
                         ...prevAllPokemons,
                         [pokemonData.id]: pokemonData
                     }));
-                })
-                .catch(error => console.error('Error fetching data for', pokemon.name, ':', error));
-        };
+                }
+            })
+            .catch(error => console.error('Error fetching data for', pokemon.name, ':', error))
+    };
 
-        const handleChange = event => {
-            setSearchText(event.target.value);
-        };
+    const handleChange = event => {
+        setSearchText(event.target.value);
+    };
 
-        const handleSubmit = () => {
-            const results = Object.values(allPokemons).filter(pokemon =>
-                pokemon.name.includes(searchText.toLowerCase())
-            );
-            setSearchResults(results);
-            
-        };
-
-        return (
-            <div>
-                <SearchHeader inputText={searchText} handleChange={handleChange} handleSubmit={handleSubmit}/>
-                {allPokemons.length === 0 ? (
-                    <div className='loading'>
-                        <img alt='loading' src="/loading.gif"/>
-                    </div>
-                ) : (
-                    <PokemonsList pokemons={searchResults.length ? searchResults : Object.values(allPokemons)}/>
-                )}
-            </div>
+    const handleSubmit = () => {
+        const results = Object.values(allPokemons).filter(pokemon =>
+            pokemon.name.includes(searchText.toLowerCase())
         );
+        setSearchResults(results);
+    };
+
+    if (allPokemons.length === 0) {
+        return (
+            <div className='loading'>
+                <img alt='loading' src="/loading.gif"/>
+            </div>
+        )
     }
-;
+
+    return (
+        <div>
+            <SearchHeader inputText={searchText} handleChange={handleChange} handleSubmit={handleSubmit}/>
+            {allPokemons.length === 0 ? (
+                <div className='loading'>
+                    <img alt='loading' src="/loading.gif"/>
+                </div>
+            ) : (
+                <PokemonsList pokemons={searchResults.length ? searchResults : Object.values(allPokemons)}/>
+            )}
+        </div>
+    );
+}
 
 export default SearchPage;
