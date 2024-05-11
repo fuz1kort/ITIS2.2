7b4faf11dbdead54f9df067e249f5737c8ba1360
fuz1kort <113291823+fuz1kort@users.noreply.github.com>
2024-05-11 11:44:41 +0300

    не работает докер

--- HomeWork4/PokemonsAPI/PokemonsAPI.Data/obj/Debug/net8.0/PokemonsAPI.Data.AssemblyInfo.cs
+++ HomeWork4/PokemonsAPI/PokemonsAPI.Data/obj/Debug/net8.0/PokemonsAPI.Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PokemonsAPI.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+355dda7356300e10c46b619e9fd9b6cc995d797a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PokemonsAPI.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PokemonsAPI.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- HomeWork4/PokemonsAPI/PokemonsAPI.WEB/obj/Debug/net8.0/PokemonsAPI.WEB.AssemblyInfo.cs
+++ HomeWork4/PokemonsAPI/PokemonsAPI.WEB/obj/Debug/net8.0/PokemonsAPI.WEB.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PokemonsAPI.WEB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+355dda7356300e10c46b619e9fd9b6cc995d797a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PokemonsAPI.WEB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PokemonsAPI.WEB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
